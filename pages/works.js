66155cdb5b616b5e4153a6ec056bdf01224710f0
Armin Sabouri <armins88@gmail.com>
2024-05-18 15:05:29 -0400

    add CAT

--- pages/works.js
+++ pages/works.js
@@ -14,6 +14,19 @@ const Works = () => (
       </Heading>
       <SimpleGrid columns={[1, 1]} gap={6}>
         <Section>
+         <WorkGridItem
+            id="cat"
+            title="Re-Enable OP_CAT (BIP-347)"
+            date="April 2024"
+            url="https://github.com/bitcoin/bips/blob/master/bip-0347.mediawiki"
+            thumbnail="/images/CAT_emoji.webp"
+            width={100}
+            height={100}
+          >
+            This BIP introduces OP_CAT as a tapscript opcode which allows the concatenation of two values on the stack. OP_CAT would be activated via a soft fork by redefining the opcode OP_SUCCESS126 (126 in decimal and 0x7e in hexadecimal). This is the same opcode value used by the original OP_CAT.
+          </WorkGridItem>
+        </Section>
+        <Section>
           <WorkGridItem
             id="frost"
             title="Frost Notebook"
